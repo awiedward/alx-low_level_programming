ebea92504651c0a5ece736b5b84538ded480bc59
awiedward <oe01.awi@gmail.com>
2022-06-05 16:24:50 -0700

    Programming is like building a multilingual puzzl

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *
  *  * main - Prints out a string to stdout.
@@ -13,13 +12,8 @@
  *      * Return: 0 if success.
  *
  *       */
-
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle");
-
 			return (0);
-
 }
