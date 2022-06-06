18d6580e0349f2651145581f0b60b12dcfb442aa
awiedward <oe01.awi@gmail.com>
2022-06-05 17:49:18 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *    * Description: this prins out string
  *     *
  *      * Return: 0 if success.
- *       */
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
